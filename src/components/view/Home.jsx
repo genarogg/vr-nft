33c0fe95326218d31e011052c811fac2eb007540
genaro gonzalez <genarrogg@gmail.com>
2023-07-18 12:00:40 -0400

    funciona todo

--- src/components/view/Home.jsx
+++ src/components/view/Home.jsx
@@ -8,15 +8,21 @@ import { imgGrigora, Grigora } from "../../configNFT/Grigora";
 import { imgSpartan, Spartan } from "../../configNFT/Spartan";
 import { imgEros, Eros } from "../../configNFT/Eros";
 
+import { Provider } from "react-redux";
+
+import store from "../../redux/store";
+
 const Home = () => {
   return (
     <>
       <Layout>
         <div className="container-all-card">
-          <NFT xConfig={Grigora()} imgNFT={imgGrigora} />
-          <NFT xConfig={Jerico()} imgNFT={imgGerico} />
-          <NFT xConfig={Spartan()} imgNFT={imgSpartan} />
-          <NFT xConfig={Eros()} imgNFT={imgEros} />
+          <Provider store={store}>
+            <NFT xConfig={Grigora()} imgNFT={imgGrigora} />
+            <NFT xConfig={Jerico()} imgNFT={imgGerico} />
+            <NFT xConfig={Spartan()} imgNFT={imgSpartan} />
+            <NFT xConfig={Eros()} imgNFT={imgEros} />
+          </Provider>
         </div>
       </Layout>
     </>
--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,4 @@ import ReactDOM from "react-dom";
 import "./css/style.css";
 import Home from "./components/view/Home";
 
-import { Provider } from "react-redux";
-
-import store from "./redux/store";
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Home />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Home />, document.getElementById("root"));
